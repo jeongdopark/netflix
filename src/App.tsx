4f2a1753313fdbab7f218a8864811dc2035fd92a
jeongdopark <jdpark0816@gmail.com>
2023-05-02 11:11:50 +0900

    style: css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { GlobalStyle } from './style/globalStyle'
+import Home from './pages/Home'
 
 function App() {
-  return <div></div>
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Home />
+    </React.Fragment>
+  )
 }
 
 export default App
